52a02b8d7ff196a7e6dc653329dd63226f3c03ae
misayaas <221250124@smail.nju.edu.cn>
2025-04-27 15:47:42 +0800

    delete useless module

--- evaluation_core/apps.py
+++
@@ -1,7 +0,0 @@
-## 应用的配置类，包含应用的基本信息
-from django.apps import AppConfig
-
-
-class EvaluationCoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "evaluation_core"
